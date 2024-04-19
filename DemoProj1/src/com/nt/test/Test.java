0827156f1aa88e5c5e3cc929e16f8eaa230655e7
TL <TL@gmail.com>
2024-04-20 00:21:58 +0530

    Dev1 Modified Test.java

--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -1,5 +1,14 @@
 package com.nt.test;
 
+import com.nt.bean.Utility;
+
 public class Test {
+	
+	//added
+	public static void main(String [] args)
+	{
+		Utility u = new Utility();
+		System.out.println(u.getSysDate());
+	}
 
 }
